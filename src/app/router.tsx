5897c23045446a5c0ace3339c254b483cb926569
Yurii-Yanovitsky <yanovitsky1998@gmail.com>
2024-09-09 19:27:43 +0300

    Add pages

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -3,19 +3,21 @@ import {
   createRoute,
   createRootRoute,
 } from "@tanstack/react-router";
+import HomePage from "../pages/HomePage";
+import AuthPage from "../pages/AuthPage";
 
 const rootRoute = createRootRoute({});
 
 const indexRoute = createRoute({
   getParentRoute: () => rootRoute,
   path: "/",
-  component: () => <div>Home</div>,
+  component: () => <HomePage />,
 });
 
 const aboutRoute = createRoute({
   getParentRoute: () => rootRoute,
   path: "/login",
-  component: () => <div>Login</div>,
+  component: () => <AuthPage />,
 });
 
 const routeTree = rootRoute.addChildren([indexRoute, aboutRoute]);
