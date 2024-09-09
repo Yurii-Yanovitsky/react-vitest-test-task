31327ab70adff71527d82c140db5526886920879
Yurii-Yanovitsky <yanovitsky1998@gmail.com>
2024-09-09 18:40:26 +0300

    Set up vitest for project

---
+++ vitest.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from "vite";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: ["./vitest.setup.ts"],
+    include: ["**/test.{ts,tsx}"],
+  },
+});
