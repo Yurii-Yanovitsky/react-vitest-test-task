b598141bffa41c1d625cb336c08fa2cc2f2c3a50
Yurii-Yanovitsky <yanovitsky1998@gmail.com>
2024-09-10 19:27:35 +0300

    Fix className combining

--- src/shared/components/Button.tsx
+++ src/shared/components/Button.tsx
@@ -9,11 +9,11 @@ import {
 type ButtonProps = PropsWithChildren<ButtonHTMLAttributes<HTMLButtonElement>>;
 
 const Button: FC<ButtonProps> = forwardRef(
-  ({ children, ...props }, ref: ForwardedRef<HTMLButtonElement>) => {
+  ({ children, className, ...props }, ref: ForwardedRef<HTMLButtonElement>) => {
     return (
       <button
         ref={ref}
-        className="w-full bg-gray-600 text-white py-2 rounded-lg font-semibold hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-800 transition-all"
+        className={`bg-gray-600 text-white py-2 rounded-lg font-semibold hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-800 transition-all ${className || ""}`}
         {...props}
       >
         {children}
--- src/shared/components/Input.tsx
+++ src/shared/components/Input.tsx
@@ -3,11 +3,11 @@ import { FC, ForwardedRef, forwardRef, InputHTMLAttributes } from "react";
 type InputProps = InputHTMLAttributes<HTMLInputElement>;
 
 const Input: FC<InputProps> = forwardRef(
-  ({ ...props }, ref: ForwardedRef<HTMLInputElement>) => {
+  ({ className, ...props }, ref: ForwardedRef<HTMLInputElement>) => {
     return (
       <input
         ref={ref}
-        className="p-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-gray-500 focus:border-transparent"
+        className={`p-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-gray-500 focus:border-transparent ${className || ""}`}
         {...props}
       />
     );
