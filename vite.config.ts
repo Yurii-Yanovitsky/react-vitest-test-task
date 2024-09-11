0e4ef7be39a17cc425797955eb68a9f4602273c8
Yurii-Yanovitsky <yanovitsky1998@gmail.com>
2024-09-11 13:51:36 +0300

    Replace swc with babel for decorators support

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      babel: {
+        babelrc: false,
+        configFile: false,
+        plugins: [
+          ["@babel/plugin-proposal-decorators", { legacy: true }],
+          "@babel/plugin-proposal-class-properties",
+          "babel-plugin-transform-typescript-metadata",
+        ],
+      },
+    }),
+  ],
 });
